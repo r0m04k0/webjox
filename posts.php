71ede18ae3ce0e28e2355f71ce47af4e91a2a81c
romo4ko <romo4ko.mail@gmail.com>
2023-05-27 19:52:26 +0300

    Added a pagination

--- posts.php
+++ posts.php
@@ -32,62 +32,100 @@
 
 
     
-<main class="m-3">
+<main class="m-4">
 
-    <form action="" method="get" enctype="multipart/form-data">
+    <form action="" class="row g-3 mb-4 justify-content-center" method="get" enctype="multipart/form-data">
 
-        <?php
+        <div class="col-md-4">   
+                <label class="control-label" for="limit">Введите количество постов на одной странице:</label>
+            <?php
+                $limit = !empty($_GET['limit']) ? $_GET['limit'] : '10';
+                echo "<input class='form-control' type='number' name='limit' id='limit' min='1' max='20' value='$limit' style='width: 80px' onchange='this.form.submit()'/>";
+            ?>   
+        </div>
 
-        $category = !empty($_GET['category']) ? $_GET['category'] : 'all';
+        <div class="col-md-3">
+            
+            <label class="control-label" for="category">Выберите кетегорию:</label>
 
-        $result = mysqli_query($connect, "SELECT DISTINCT category FROM categories" );
-        echo "<select class='mb-3' name='category' onchange='this.form.submit()'>";
+            <?php
 
-        if ($category != 'all') {
-            echo "<option value='$category' selected>$category</option>";
-            echo "<option value = 'all'> Все теги </option>";    
-        }
-        else echo "<option value = 'all' selected> Все теги </option>";
+            $category = !empty($_GET['category']) ? $_GET['category'] : 'all';
 
-        while ($row=mysqli_fetch_array($result)) {
-            if ($row['category'] != $category) {
-                echo "<option value = ".$row['category']."> ".$row['category']." </option>";
+            $result = mysqli_query($connect, "SELECT DISTINCT category FROM categories" );
+            echo "<select class='form-select' id='category' name='category' onchange='this.form.submit()'>";
+
+            if ($category != 'all') {
+                echo "<option value='$category' selected>$category</option>";
+                echo "<option value = 'all'> Все теги </option>";    
             }
-        }
-        echo "</select>";
-        ?>
+            else echo "<option value = 'all' selected> Все теги </option>";
+
+            while ($row=mysqli_fetch_array($result)) {
+                if ($row['category'] != $category) {
+                    echo "<option value = ".$row['category']."> ".$row['category']." </option>";
+                }
+            }
+            echo "</select>";
+            ?>
+        </div>
 
     </form>
 
-    <div class="row row-cols-1 row-cols-md-5 g-4">
-
     <?php
 
-    if (isset($_GET)) {
-
-        $category = !empty($_GET['category']) ? $_GET['category'] : 'all';
-
-        $query = "SELECT `title` AS `title`,
-                `caption` AS `caption`,
-                `created` AS `created`,
-                `images`.`path` AS `image`,
-                `posts`.`id` AS `id`,
-                `categories`.`category` AS `category`
-                FROM `posts`, `images`, `categories` 
-                WHERE `posts`.`image` = `images`.`id` AND `posts`.`category` = `categories`.`id`";
-            
-        if ($category != 'all') {
-            $query .= " AND `categories`.`category` = '$category' ";
-        }
-        $query .= " ORDER BY `created` DESC ;";
-    }
-
-    $result = mysqli_query($connect, $query);
-
-    if (!($result->num_rows > 0)) {
-        echo "<p>Постов нет</p>";
+    $category = !empty($_GET['category']) ? $_GET['category'] : 'all';
+    $limit = !empty($_GET['limit']) ? $_GET['limit'] : 10;
+    $page = !empty($_GET['page']) ? $_GET['page'] : 1;
+    if ($page == 1) {
+        $offset = 0;
     }
     else {
+        $offset = $offset = ($page-1) * $limit;
+    }
+    
+
+    $query = "SELECT `title` AS `title`,
+            `caption` AS `caption`,
+            `created` AS `created`,
+            `images`.`path` AS `image`,
+            `posts`.`id` AS `id`,
+            `categories`.`category` AS `category`
+            FROM `posts`, `images`, `categories` 
+            WHERE `posts`.`image` = `images`.`id` AND `posts`.`category` = `categories`.`id`";
+        
+    if ($category != 'all') {
+        $query .= " AND `categories`.`category` = ? ";
+    }
+
+    $stmt = $connect->prepare($query);
+    if ($category != 'all') {
+        $stmt->bind_param("s", $category);
+    }
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $count = $result->num_rows;
+
+    $query .= " ORDER BY `created` DESC LIMIT ? OFFSET ? ;";
+
+    $stmt = $connect->prepare($query);
+    if ($category != 'all') {
+        $stmt->bind_param("sii", $category, $limit, $offset);
+    } 
+    else {
+        $stmt->bind_param("ii", $limit, $offset);
+    }
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $currentCount = $result->num_rows;
+
+    if (!($currentCount > 0)) {
+        echo "<p class='row justify-content-center mt-5'>Постов нет</p>";
+    }
+
+    else {
+
+        echo "<div class='row row-cols-1 row-cols-md-5 g-4 justify-content-center'>";
         
         while ($row=mysqli_fetch_array($result)) {
             
@@ -109,29 +147,65 @@
                 </div>";
 
         }
+
+        echo "</div>";
+
+        $next = "posts.php?page=".($page+1)."&limit=$limit&category=$category";
+        $previous = "posts.php?page=".($page-1)."&limit=$limit&category=$category";
+
+        echo "<nav aria-label='...' class='mt-5'>
+        <ul class='pagination justify-content-center'>";
+        if ($page == 1) {
+            echo "<li class='page-item disabled'>
+            <a class='page-link'>Previous</a>
+            </li>";
+        }
+        else {
+            echo "<li class='page-item'>
+            <a class='page-link' href='$previous'>Previous</a>
+            </li>";
+        }
+        if ($count <= $limit) {
+            echo "<li class='page-item active'><a class='page-link' href='#'>1</a></li>
+            <li class='page-item disabled'>
+            <a class='page-link' href='#'>Next</a>
+            </li>";
+        }
+        else {
+            for ($i=1; $i <= ceil($count/$limit); $i++) {
+                $current = "posts.php?page=$i&limit=$limit&category=$category"; 
+                if ($page == $i) {
+                    echo "<li class='page-item active' aria-current='page'>
+                <a class='page-link' href='$current'>$i</a>
+                </li>";    
+                }
+                else {
+                    echo "<li class='page-item' aria-current='page'>
+                    <a class='page-link' href='$current'>$i</a>
+                    </li>";
+                }
+            }
+            if ($page == ceil($count/$limit)) {
+                echo "<li class='page-item disabled'>
+            <a class='page-link' href='#'>Next</a>
+            </li>";    
+            }
+            else {
+                echo "<li class='page-item'>
+            <a class='page-link' href='$next'>Next</a>
+            </li>";
+            }
+            
+        }
+        echo"</ul>
+        </nav>";
+
     }
 
-    
                     
     ?>
 
-    </div>
-
-    <nav aria-label="..." class="mt-5">
-    <ul class="pagination justify-content-center">
-        <li class="page-item disabled">
-        <a class="page-link">Previous</a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item active" aria-current="page">
-        <a class="page-link" href="#">2</a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item">
-        <a class="page-link" href="#">Next</a>
-        </li>
-    </ul>
-    </nav>
+    
 
 </main>    
 
